ad3d937cd73142f43e95ec02dc53d2926630e944
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2022-04-27 11:56:23 +0400

    consistent constant case syntax

--- src/emailAlert.ts
+++ src/emailAlert.ts
@@ -6,27 +6,27 @@ import { error, log } from './debug'
 // When API key is not set console logs a warning: API key does not start with "SG.".
 // Send can still be attempted but we will most likely fail with 401 error response not authorized
 // unless ip address based authentication is configured.
-const apiKey = process.env.SENDGRID_API_KEY
+const SENDGRID_API_KEY = process.env.SENDGRID_API_KEY
 // Validated sender address
-const fromEmail = process.env.ALERT_FROM_EMAIL
+const ALERT_FROM_EMAIL = process.env.ALERT_FROM_EMAIL
 // For multiple emails messages use a comma separated list. It is preferable
 // to use a single address of a distribution list to send alerts to multiple users if possible.
-const toEmail = process.env.ALERT_TO_EMAIL
+const ALERT_TO_EMAIL = process.env.ALERT_TO_EMAIL
 
-sendgrid.setApiKey(apiKey || '')
+sendgrid.setApiKey(SENDGRID_API_KEY || '')
 
 export const sendEmailAlert = (message: string) => {
-    if (!(apiKey && fromEmail && toEmail)) {
+    if (!(SENDGRID_API_KEY && ALERT_FROM_EMAIL && ALERT_TO_EMAIL)) {
         log('Email Alerts not configured, not sending email alert.')
         return
     }
 
-    const emails = toEmail.split(',')
+    const emails = ALERT_TO_EMAIL.split(',')
 
     const messages = emails.map((to) => {
         return {
             to,
-            from: fromEmail,
+            from: ALERT_FROM_EMAIL,
             subject: 'Member Faucet Alert',
             text: message,
             html: `<span>${message}</span>`,
