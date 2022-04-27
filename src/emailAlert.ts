3aac3738a6e6bd80ab58aeee552937aa797ef196
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2022-04-27 11:29:46 +0400

    alerts: configurable from email, code cleanup, support multiple destination emails

--- src/emailAlert.ts
+++ src/emailAlert.ts
@@ -1,35 +1,45 @@
-const sendgrid = require('@sendgrid/mail')
-
+import sendgrid from '@sendgrid/mail'
 import { error, log } from './debug'
 
-const api_key = process.env.SENDGRID_API_KEY
-const alert_email = process.env.ALERT_EMAIL
+// Reference https://docs.sendgrid.com/for-developers/sending-email/quickstart-nodejs
+
+// When API key is not set console logs a warning: API key does not start with "SG.".
+// Send can still be attempted but we will most likely fail with 401 error response not authorized
+// unless ip address based authentication is configured.
+const apiKey = process.env.SENDGRID_API_KEY
+// Validated sender address
+const fromEmail = process.env.ALERT_FROM_EMAIL
+// For multiple emails messages use a comma separated list. It is preferable
+// to use a single address of a distribution list to send alerts to multiple users if possible.
+const toEmail = process.env.ALERT_TO_EMAIL
+
+sendgrid.setApiKey(apiKey || '')
 
 export const sendEmailAlert = (message: string) => {
-    if (!api_key || !alert_email) {
+    if (!(apiKey && fromEmail && toEmail)) {
         log('Email Alerts not configured, not sending email alert.')
         return
     }
 
-    sendgrid.setApiKey(api_key)
+    const emails = toEmail.split(',')
 
-    const msg = {
-        to: alert_email,
-        from: 'mokhtar@jsgenesis.com',
-        subject: 'Member Faucet Alert',
-        text: message,
-        html: `<span>${message}</span>`,
-    }
+    const messages = emails.map((to) => {
+        return {
+            to,
+            from: fromEmail,
+            subject: 'Member Faucet Alert',
+            text: message,
+            html: `<span>${message}</span>`,
+        }
+    })
 
-    sendgrid.send(msg)
+    const isMultiple = messages.length > 1
+
+    sendgrid.send(messages, isMultiple)
         .then(() => {
             log('Sent Email Alert!')
         })
         .catch((err: any) => {
             error('Failed Sending Email Alert', err)
         })
-}
-
-export const send_test_alert = () => {
-    return sendEmailAlert('test alert message')
 }
