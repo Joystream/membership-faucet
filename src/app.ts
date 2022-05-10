aaca2afc1a41bfa61aa4b7fcab9348db9f979d51
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2022-05-10 10:57:10 +0400

    global rate limit on register api endpoint

--- src/app.ts
+++ src/app.ts
@@ -6,14 +6,22 @@ import { register } from './register'
 import { AnyJson } from "@polkadot/types/types";
 import bodyParser from 'body-parser';
 import locks from "locks";
-import rateLimit from 'express-rate-limit';
+import expressRateLimit from 'express-rate-limit';
+import { InMemoryRateLimiter } from "rolling-rate-limiter";
 
-const registerLimiter = rateLimit({
+// per IP rate limit
+const expressIpLimiter = expressRateLimit({
   windowMs: 48 * 60 * 60 * 1000,
   max: 3, // Limit each IP to N requests per `window`
   standardHeaders: false, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: true, // Disable the `X-RateLimit-*` headers
 })
+
+// global rate limit
+const rollingGlobalLimiter = new InMemoryRateLimiter({
+  interval: 1 * 60 * 60 * 1000, // milliseconds
+  maxInInterval: 10,
+});
 
 const app = express();
 const port = parseInt(process.env.PORT || '3002');
@@ -26,6 +34,7 @@ app.use(cors());
 
 // Configure number of hops behind reverse proxy
 app.set('trust proxy', 1)
+
 // ip endpoint to show request ip address for debugging
 app.get('/ip', (request, response) => response.send(request.ip))
 
@@ -48,37 +57,49 @@ app.get("/status", async (req, res) => {
   }
 });
 
-app.post("/register", registerLimiter, async (req, res) => {
-  await joy.init
-
-  // if node is still syncing .. don't process request
-  const { isSyncing } = await joy.api.rpc.system.health()
-  if (isSyncing.isTrue) {
-    res.setHeader("Content-Type", "application/json");
-    res.status(500).send({
-      error: 'NodeNotReady'
+app.post("/register",
+  expressIpLimiter,
+  (req, res, next) => {
+    rollingGlobalLimiter.limit('global').then((wasBlocked) => {
+      if (wasBlocked) {
+        return res.status(429).send("Too many requests");
+      } else {
+        return next();
+      }
     });
-    return
-  }
+  },
+  async (req, res) => {
+    await joy.init
 
-  const callback = (result: AnyJson, statusCode: number) => {
-    processingRequest.unlock();
-    res.setHeader("Content-Type", "application/json");
-    res.status(statusCode).send(result);
-  }
-
-  const { account, handle, avatar, about, name } = req.body
-
-  processingRequest.lock(async () => {
-    try {
-      await register(joy, account, handle, name, avatar, about, callback);
-    } catch (err) {
-      processingRequest.unlock();
-      log(err)
-      res.status(500).end()
+    // if node is still syncing .. don't process request
+    const { isSyncing } = await joy.api.rpc.system.health()
+    if (isSyncing.isTrue) {
+      res.setHeader("Content-Type", "application/json");
+      res.status(500).send({
+        error: 'NodeNotReady'
+      });
+      return
     }
-  })
-});
+
+    const callback = (result: AnyJson, statusCode: number) => {
+      processingRequest.unlock();
+      res.setHeader("Content-Type", "application/json");
+      res.status(statusCode).send(result);
+    }
+
+    const { account, handle, avatar, about, name } = req.body
+
+    processingRequest.lock(async () => {
+      try {
+        await register(joy, account, handle, name, avatar, about, callback);
+      } catch (err) {
+        processingRequest.unlock();
+        log(err)
+        res.status(500).end()
+      }
+    })
+  }
+);
 
 app.listen(port, () => {
   log(`server started at http://localhost:${port}`);
